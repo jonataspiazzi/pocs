8394bd7fd9900217913617d1e1e9edd760f5db2c
Jonatas Piazzi <piazzi_7@hotmail.com>
2020-11-20 02:40:05 -0300

    Finished the basic example using mobx and hooks

--- react/basic-mobx-hooks/src/App.tsx
+++ react/basic-mobx-hooks/src/App.tsx
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { FunctionComponent, useContext, createContext, useState, FormEvent } from 'react';
+import { useLocalObservable, Observer } from 'mobx-react';
+import { makeAutoObservable } from 'mobx';
 import './App.css';
+
+class BugsStore {
+  bugs: string[];
+
+  constructor() {
+    this.bugs = [];
+  }
+
+  addBug(bug: string) {
+    this.bugs.push(bug);
+  }
+
+  get bugsCount() {
+    return this.bugs.length;
+  }
+}
+
+const BugsContext = createContext<BugsStore>(null);
+
+const StoreProvider: FunctionComponent = ({ children }) => {
+  const store = useLocalObservable(() => {
+    const bugsStore = new BugsStore();
+    makeAutoObservable(bugsStore);
+    return bugsStore;
+  });
+
+  return <BugsContext.Provider value={store}>{children}</BugsContext.Provider>;
+}
+
+const BugsHeader = () => {
+  const store = useContext(BugsContext);
+
+  return <Observer>
+    {() => (
+      <h1>{store.bugsCount} Bugs!</h1>
+    )}
+  </Observer>;
+}
+
+const BugsList = () => {
+  const store = useContext(BugsContext);
+
+  return <Observer>
+    {() => (
+      <ul>
+        {store.bugs.map(bug => (
+          <li key={bug}>{bug}</li>
+        ))}
+      </ul>
+    )}
+  </Observer>;
+}
+
+const BugsForm = () => {
+  const store = useContext(BugsContext);
+  const [bug, setBug] = useState('');
+
+  function submit(e: FormEvent) {
+    store.addBug(bug);
+    setBug('');
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <input type="text" value={bug} onChange={e => setBug(e.target.value)} />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
+    <StoreProvider>
+      <div className="App">
+        <BugsHeader />
+        <BugsList />
+        <BugsForm />
+      </div>
+    </StoreProvider>
   );
 }
 
